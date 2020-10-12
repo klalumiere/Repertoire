8121fd55bd2826ec085c60424858420f4fd07f69
Kevin Lalumiere <kevin.lalumiere@gmail.com>
2020-10-12 15:28:52 -0400

    Read song file

--- app/src/main/java/com/example/repertoire/SongActivity.kt
+++ app/src/main/java/com/example/repertoire/SongActivity.kt
@@ -1,8 +1,12 @@
 package com.example.repertoire
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_song.*
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
 
 class SongActivity : AppCompatActivity() {
     companion object {
@@ -18,6 +22,31 @@ class SongActivity : AppCompatActivity() {
         val bundle = intent.extras!!
         val songName = bundle[SONG_NAME] as String
         val songUriAsString = bundle[SONG_URI_AS_STRING] as String
-        song_text_view.text = "$songName $songUriAsString"
+        val songContent = readSongFile(Uri.parse(songUriAsString)) // TODO: This should not be on the main thread
+        song_text_view.text = "$songName $songContent"
+    }
+
+
+    private fun readSongFile(uri: Uri): String {
+        try {
+            return readSongFileUnsafe(uri)
+        }
+        catch(e: IOException) // Includes FileNotFoundException
+        { }
+        return "Cannot read file \uD83D\uDE1E" // sad emoji
+    }
+
+    private fun readSongFileUnsafe(uri: Uri): String {
+        val stringBuilder = StringBuilder()
+        contentResolver.openInputStream(uri)?.use { inputStream ->
+            BufferedReader(InputStreamReader(inputStream)).use { reader ->
+                var line: String? = reader.readLine()
+                while (line != null) {
+                    stringBuilder.append(line)
+                    line = reader.readLine()
+                }
+            }
+        }
+        return stringBuilder.toString()
     }
 }

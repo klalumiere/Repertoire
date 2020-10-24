edd22df867439759e80added447c44082b99e39e
Kevin Lalumiere <kevin.lalumiere@gmail.com>
2020-10-24 17:23:45 -0400

    Add test for SongAdapter

--- app/src/test/java/com/example/repertoire/SongRecyclerViewTest.kt
+++ app/src/test/java/com/example/repertoire/SongRecyclerViewTest.kt
@@ -1,10 +1,12 @@
 package com.example.repertoire
 
 import android.os.Looper.getMainLooper
+import androidx.recyclerview.selection.SelectionTracker
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.launchActivity
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import com.nhaarman.mockitokotlin2.*
 import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
@@ -16,7 +18,7 @@ import org.robolectric.annotation.Config
 
 @RunWith(AndroidJUnit4::class)
 @Config(sdk = [28]) // >= 29 not supported by Android Studio right now
-class SongRecyclerViewTest {
+class SongViewHolderTest {
     private val song = Song(
         uri = "content://arbitrary/uri",
         name = "Pearl Jam - Black"
@@ -26,11 +28,8 @@ class SongRecyclerViewTest {
     fun bindHolderSetTextViewed() {
         val scenario = launchActivity<MainActivity>()
         scenario.onActivity { activity ->
-            val group = activity.findViewById<RecyclerView>(R.id.song_list_view)
-            val holder = SongViewHolder.create(group)
-
+            val holder = createSongViewHolder(activity)
             holder.bind(song)
-
             assertEquals(song.name, holder.getTextViewed())
         }
     }
@@ -39,11 +38,8 @@ class SongRecyclerViewTest {
     fun bindHolderSetContentUri() {
         val scenario = launchActivity<MainActivity>()
         scenario.onActivity { activity ->
-            val group = activity.findViewById<RecyclerView>(R.id.song_list_view)
-            val holder = SongViewHolder.create(group)
-
+            val holder = createSongViewHolder(activity)
             holder.bind(song)
-
             assertEquals(song.uri, holder.getContentUri())
         }
     }
@@ -52,8 +48,7 @@ class SongRecyclerViewTest {
     fun canSetActivated() {
         val scenario = launchActivity<MainActivity>()
         scenario.onActivity { activity ->
-            val group = activity.findViewById<RecyclerView>(R.id.song_list_view)
-            val holder = SongViewHolder.create(group)
+            val holder = createSongViewHolder(activity)
 
             holder.setActivated(false)
             assertFalse(holder.isViewActivated())
@@ -67,14 +62,21 @@ class SongRecyclerViewTest {
     fun holderSelectionKeyIsUri() {
         val scenario = launchActivity<MainActivity>()
         scenario.onActivity { activity ->
-            val group = activity.findViewById<RecyclerView>(R.id.song_list_view)
-            val holder = SongViewHolder.create(group)
-
+            val holder = createSongViewHolder(activity)
             holder.bind(song)
-
             assertEquals(holder.getItemDetails().selectionKey, song.uri)
         }
     }
+
+     @Test
+    fun holderItemDetailsPositionIsHolderAdapterPosition() {
+        val scenario = launchActivity<MainActivity>()
+        scenario.onActivity { activity ->
+            val holder = createSongViewHolder(activity)
+            assertEquals(holder.adapterPosition, holder.getItemDetails().position)
+        }
+    }
+
 
     @Before
     fun allowMainThreadQueriesInDatabase() {
@@ -144,4 +146,78 @@ class SongItemCallbackTestTest {
         )
         assertFalse(callback.areContentsTheSame(song,song2))
     }
+}
+
+@RunWith(AndroidJUnit4::class)
+@Config(sdk = [28]) // >= 29 not supported by Android Studio right now
+class SongAdapterTest {
+    private val song = Song(
+        uri = "content://arbitrary/uri",
+        name = "Pearl Jam - Black"
+    )
+
+    @Test
+    fun adapterOnBindViewHolder_BindsHolder() {
+        val scenario = launchActivity<MainActivity>()
+        scenario.onActivity { activity ->
+            val holder = createSongViewHolder(activity)
+            createSongAdapter(song).onBindViewHolder(holder,0)
+            assertEquals(song.name, holder.getTextViewed())
+        }
+    }
+
+    @Test
+    fun adapterOnBindViewHolder_SetActivatedToTrackerIsSelected() {
+        val scenario = launchActivity<MainActivity>()
+        scenario.onActivity { activity ->
+            val holder = createSongViewHolder(activity)
+            listOf(false,true).forEach { booleanValue ->
+                val mockedTracker = mock<SelectionTracker<String>> {
+                    on {
+                        isSelected(same(song.uri))
+                    } doReturn booleanValue
+                }
+
+                createSongAdapter(song, mockedTracker).onBindViewHolder(holder,0)
+
+                assertEquals(booleanValue, holder.isViewActivated())
+            }
+        }
+    }
+
+    @Test
+    fun adapterOnCreateViewHolder_createsSongViewHolder() {
+        val scenario = launchActivity<MainActivity>()
+        scenario.onActivity { activity ->
+            val group = activity.findViewById<RecyclerView>(R.id.song_list_view)
+            assertNotEquals(null, createSongAdapter(song).onCreateViewHolder(group,0))
+        }
+    }
+
+
+    @Before
+    fun allowMainThreadQueriesInDatabase() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        AppDatabase.getInstanceAllowingMainThreadQueriesForTests(context)
+    }
+    @After
+    fun preventExceptions() {
+        shadowOf(getMainLooper()).idle()
+
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        AppDatabase.getInstanceAllowingMainThreadQueriesForTests(context).close()
+    }
+}
+
+
+private fun createSongAdapter(song: Song, trackerRhs: SelectionTracker<String> = mock()): SongAdapter {
+    return SongAdapter().apply {
+        submitList(listOf(song))
+        tracker = trackerRhs
+    }
+}
+
+private fun createSongViewHolder(activity: MainActivity): SongViewHolder {
+    val group = activity.findViewById<RecyclerView>(R.id.song_list_view)
+    return SongViewHolder.create(group)
 }

b2775e67b578b9715d93db419eb03cb2f78e5e51
Kevin Lalumiere <kevin.lalumiere@gmail.com>
2020-10-25 16:19:53 -0400

    Add EndToEndTest file

---
+++ app/src/androidTest/java/com/example/repertoire/EndToEndTest.kt
@@ -0,0 +1,14 @@
+package com.example.repertoire
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class EndToEndTest {
+    @Test
+    fun canAddSong() {
+        assertEquals(2, 1+1)
+    }
+}

7526a08e4cf0ea609635cdfff767c5c6e0cdbc59
Kevin Lalumiere <kevin.lalumiere@gmail.com>
2020-10-27 20:27:59 -0400

    * Add canSelect Song

--- app/src/androidTest/java/com/example/repertoire/EndToEndTest.kt
+++ app/src/androidTest/java/com/example/repertoire/EndToEndTest.kt
@@ -8,29 +8,32 @@ import androidx.activity.result.contract.ActivityResultContract
 import androidx.core.app.ActivityOptionsCompat
 import androidx.lifecycle.Lifecycle
 import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.longClick
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
 import androidx.test.espresso.matcher.BoundedMatcher
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.hamcrest.Description
 import org.hamcrest.Matcher
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class EndToEndTest {
+    private val assetUri = Uri.parse("file:///android_asset/Happy%20Birthday.md")
+    private val context = ApplicationProvider.getApplicationContext<Context>()
+
     @Test
     fun canAddSong() {
-        val assetUri = Uri.parse("file:///android_asset/Happy%20Birthday.md")
-        val context = ApplicationProvider.getApplicationContext<Context>()
         val scenario = launchActivity<MainActivity>()
 
-        AppDatabase.getInstance(context).songDao().deleteAll()
         scenario.moveToState(Lifecycle.State.CREATED)
         scenario.onActivity { activity ->
             activity.songViewModel.repository.injectContentResolverForTests(
@@ -45,18 +48,38 @@ class EndToEndTest {
         onView(withId(R.id.song_list_view))
             .check(matches(atPosition(0, withText("Happy Birthday"))))
     }
-}
+
+    @Test
+    fun canSelectSong() {
+        val scenario = launchActivity<MainActivity>()
+        addSong(scenario)
+
+        onView(withId(R.id.song_list_view))
+            .perform(actionOnItemAtPosition<SongViewHolder>(0, longClick()))
+
+        onView(withId(R.id.song_list_view))
+            .check(matches(atPosition(0, isActivated())))
+    }
 
 
-private fun createFakeActivityResultRegistry(assetUri: Uri): ActivityResultRegistry {
-    return object : ActivityResultRegistry() {
-        override fun <I, O> onLaunch(requestCode: Int, contract: ActivityResultContract<I, O>,
-            input: I, options: ActivityOptionsCompat?
-        ) {
-            dispatchResult(requestCode, listOf(assetUri))
+    @Before
+    fun clearDatabase() {
+        AppDatabase.getInstance(context).songDao().deleteAll()
+    }
+
+    private fun addSong(scenario: ActivityScenario<MainActivity>) {
+        scenario.moveToState(Lifecycle.State.CREATED)
+        scenario.onActivity { activity ->
+            activity.songViewModel.repository.injectContentResolverForTests(
+                AssetContentResolver(context))
+            activity.injectActivityResultRegistryForTest(
+                createFakeActivityResultRegistry(assetUri))
         }
+        scenario.moveToState(Lifecycle.State.RESUMED)
+        onView(withId(R.id.addSongsFAB)).perform(click())
     }
 }
+
 
 private fun atPosition(position: Int, itemMatcher: Matcher<View?>): Matcher<View?>? {
     return object : BoundedMatcher<View?, RecyclerView>(RecyclerView::class.java) {
@@ -69,6 +92,28 @@ private fun atPosition(position: Int, itemMatcher: Matcher<View?>): Matcher<View
             val viewHolder = view.findViewHolderForAdapterPosition(position)
                 ?: return false
             return itemMatcher.matches(viewHolder.itemView)
+        }
+    }
+}
+
+private fun createFakeActivityResultRegistry(assetUri: Uri): ActivityResultRegistry {
+    return object : ActivityResultRegistry() {
+        override fun <I, O> onLaunch(requestCode: Int, contract: ActivityResultContract<I, O>,
+            input: I, options: ActivityOptionsCompat?
+        ) {
+            dispatchResult(requestCode, listOf(assetUri))
+        }
+    }
+}
+
+private fun isActivated(): Matcher<View?> {
+    return object : BoundedMatcher<View?, View>(View::class.java) {
+        override fun describeTo(description: Description) {
+            description.appendText("is activated.")
+        }
+
+        override fun matchesSafely(view: View): Boolean {
+            return view.isActivated
         }
     }
 }
